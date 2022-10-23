b773149e2464db4487c2f6945f8d657bdae02a95
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-22 22:02:19 -0500

    Create router and first route

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const port = 4000;
+
+app.get( '/', ( req, res ) => {
+
+  res.json({
+    ok: true
+  });
+});
+
+app.listen( port, () => {
+  console.log(`Server running ${ port }`);
+});
