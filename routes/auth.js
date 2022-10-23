96b5ee0351a343d63f5c935b0ae61425ecb2a44b
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-23 10:30:35 -0500

    express validators

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,27 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, loginUser, reValidateToken } = require('../controllers/auth');
 
-router.post( '/', createUser );
+router.post(
+  '/',
+  [
+    check( 'name', 'El nombre es obligatorio' ).not().isEmpty(),
+    check( 'email', 'El email es obligatorio' ).not().isEmail(),
+    check( 'password', 'El password debe ser de 6 caracteres' ).isLength({ min: 6 }),
+  ],
+  createUser );
 
-router.post( '/login', loginUser );
+router.post(
+  '/login',
+  [
+    check( 'email', 'El email es obligatorio' ).not().isEmail(),
+    check( 'password', 'El password debe ser de 6 caracteres' ).isLength({ min: 6 }),
+  ],
+  loginUser );
 
 router.get( '/renew', reValidateToken );
 
