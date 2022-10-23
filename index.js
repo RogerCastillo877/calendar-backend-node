378b6005be9f555e240da4e1f2e994838bd2e2f4
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-23 10:11:10 -0500

    get information form POST body

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.use( express.static( 'public' ) );
 // Routes
 app.use( '/api/auth', require( './routes/auth' ) );
 
+// Read and parse information from body
+app.use( express.json() );
+
 // To listening requests
 app.listen( process.env.PORT, () => {
   console.log(`Server running port: ${ process.env.PORT }`);
