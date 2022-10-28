dbf54b4905e2d7875a59a0a26c9e482183fcd9ab
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-28 16:45:21 -0500

    validate fields

--- routes/events.js
+++ routes/events.js
@@ -4,15 +4,29 @@
 */
 
 const { Router } = require('express');
-const { getEvents, createEvent, updateEvent, deleteteEvent } = require('../controllers/events');
+const { check } = require('express-validator');
+
+const { isDate } = require('../helpers/isDate');
+const { fieldsValidator } = require('../middlewares/fields-validator');
 const { validateJWT } = require('../middlewares/validate-jws');
+const { getEvents, createEvent, updateEvent, deleteteEvent } = require('../controllers/events');
 
 const router = Router();
+
 router.use( validateJWT );
 
 router.get( '/', getEvents );
 
-router.post( '/', createEvent );
+router.post(
+  '/', 
+  [
+    check( 'title', 'Titulo es obligatorio' ).not().isEmpty(),
+    check( 'start', 'Fecha inicio es obligatorio' ).custom( isDate ),
+    check( 'end', 'Fecha de fin es obligatorio' ).custom( isDate ),
+    fieldsValidator
+  ],
+  createEvent
+);
 
 router.put( '/:id', updateEvent );
 
