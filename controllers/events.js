1dba2b4a0cc0e24ca8755426ffeeb92052e896d6
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-31 13:37:38 -0500

    Update event

--- controllers/events.js
+++ controllers/events.js
@@ -35,12 +35,48 @@ const createEvent = async( req, res = response ) => {
 
 };
 
-const updateEvent = ( req, res = response) => {
+const updateEvent = async( req, res = response) => {
+
+  const eventId = req.params.id;
   
-  res.json({
-    ok: true,
-    msg: 'updateEvents'
-  });
+  try {
+
+    const event = await Event.findById( eventId );
+    const uid = req.uid;
+
+    if( !event ) {
+      res.status( 404 ).json({
+        ok: false,
+        msg: "No existe por ese id"
+      })
+    };
+
+    if( event.user.toString() !== uid ) {
+       return res.status( 401 ).json({
+        ok: false,
+        msg: "No tiene privilegio de editar el evento"
+      });
+    };
+
+    const newEvent = {
+      ...req.body,
+      user: uid
+    };
+
+    const updatedEvent = await Event.findByIdAndUpdate( eventId, newEvent, { new: true } );
+
+    res.json({
+      ok: true,
+      event: updatedEvent
+    })
+    
+  } catch (error) {
+    console.log(error);
+    res.status( 500 ).json({
+      ok: false,
+      msg: "Contacte al administrador"
+    });  
+  }
 };
 
 const deleteteEvent = ( req, res = response) => {
