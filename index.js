46af6866de025179f62dc5beec2d76818d741ad4
RogerCastillo877 <roger.castillo877@gmail.com>
2022-10-31 23:07:26 -0500

    ready1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require( 'dotenv' ).config();
-const cors = require('cors')
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 
 // Express server creation
